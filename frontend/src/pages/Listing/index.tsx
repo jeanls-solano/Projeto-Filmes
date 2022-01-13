f9988f927d20e2fd5de2734f1ceedb7f93a31da7
jeanls-solano <jeanlucaseh10@gmail.com>
2022-01-13 16:09:08 -0400

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,10 @@
-function Listing(){
-    return(<h1>Página de listagem</h1>);
+import Pagination from "components/Pagination";
+
+
+function Listing() {
+    return (
+        <Pagination></Pagination>
+    );
 }
 
 export default Listing;
